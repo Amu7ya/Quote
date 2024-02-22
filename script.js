54c00730d92e1ecbea943a4ed9cb341125c3a4ed
Amu7ya <borntod4@gmail.com>
2024-02-22 22:19:43 +0545

    Quotes

---
+++ script.js
@@ -0,0 +1,29 @@
+let quotes = document.querySelector("select");
+let paraCategory = document.querySelector(".category");
+let paraAuthor = document.querySelector(".author");
+let paraQuote = document.querySelector(".quote");
+const url = "https://api.api-ninjas.com/v1/quotes?category=";
+const option = {
+  method: "GET",
+  headers: { "X-Api-Key": "dQMC4aTvyAwRVoUbOJGmtQ==Z9uVGyvGneZ7r7BI" },
+};
+const fetchData = async (url) => {
+  const response = await fetch(url, option);
+  const data = await response.json();
+  paraCategory.innerText = data[0].category;
+  paraAuthor.innerText = data[0].author;
+  paraQuote.innerText = data[0].quote;
+  console.log(data[0]);
+  console.log(data[0].author);
+  console.log(data[0].quote);
+  console.log(data[0].category);
+};
+quotes.addEventListener("change", (evt) => {
+  quoteCategories(evt.target);
+});
+
+const quoteCategories = (element) => {
+  let value = element.value;
+  let newurl = `https://api.api-ninjas.com/v1/quotes?category=${value}`;
+  fetchData(newurl);
+};
